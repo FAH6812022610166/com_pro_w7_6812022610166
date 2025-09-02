3361926e5e04e34e611f0a0105551c11427a53bd
FAH6812022610166 <s6812022610166@email.kmutnb.ac.th>
2025-09-02 23:11:24 +0700

    Add files via upload

---
+++ coding_w07_01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int score;     // กำหนดคะแนนสอบ midterm
+    float final_score;  // ประกาศตัวแปรสำหรับเก็บคะแนนรวม
+
+    printf("Enter your score:  "); // แสดงผลให้รับค่าคะแนนสอบของผู้ใช้
+    scanf("%d", &score);  // รับค่าคะแนนสอบของผู้ใช้ผ่านแป้นพิมพ์
+
+    if (score >= 50) {
+        final_score = score*1.05;   // ถ้าคะแนนมากกว่าหรือเท่ากับ 50 จะบวกเพิ่มอีก 5%
+        printf("%.2f", &final_score);
+
+    } else {
+        final_score = score;   // ถ้าคะแนนน้อยกว่า 50 ไม่บวกเพิ่ม
+    }
+
+    printf("Final score: %.2f\n", final_score);  // แสดงคะแนนรวม
+    printf("End of evaluation\n");   // แสดงข้อความจบการประเมิน 
+
+    return 0;
+} 
